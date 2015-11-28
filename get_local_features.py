4d19cccb89eff2084e75d7d8d076765804ec8ce3
JoshuaMolina <joshua.1994@hotmail.es>
2015-11-28 18:36:06 +0100

    get_local_features definitivo

---
+++ get_local_features.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import os
+import cv2
+import numpy as np
+import params 
+
+def get_local_features(params, img):
+ 
+    #Leemos una imagen
+    image = cv2.imread(img)
+    
+    #Hacemos más pequeña la imagen
+    res = cv2.resize(image ,(500,500))
+    
+    # Iniciamos el detector SIFT
+    sift = cv2.SIFT()
+    
+    #Guardamos los puntos de interés y sus respectivos descriptores
+    kp, des=sift.detectAndCompute(res,None,100)
+    
+    #Devolvemos los descriptores para una imagen
+    return des
+
+  
+
+
+   
+    
+   
+    
+    
+    
+    
